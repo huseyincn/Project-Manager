5cdc301bbbfc30768327deb0121ad24ce159656b
huseyincn <huseyin.can-@hotmail.com>
2023-12-14 00:35:21 +0300

    Add project files.

---
+++ finalll.Module/BusinessObjects/ProjeYurutucusu.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace finalll.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    [Persistent("PROJE_YURUTUCUSU")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class ProjeYurutucusu : XPObject
+    { 
+        public ProjeYurutucusu(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+        }
+
+        private string _calisanIsmi;
+        private string _email;
+        private DateTime _dogumGunu;
+        private string _adres;
+        private string _phoneNumber;
+
+        [XafDisplayName("Çalışan İsmi"), ToolTip("Çalışanın Tam Adı")]
+        [RuleRequiredField(DefaultContexts.Save, CustomMessageTemplate = "Bu alan boş olamaz")]
+        public string CalisanIsmi
+        {
+            get { return _calisanIsmi; }
+            set { SetPropertyValue(nameof(CalisanIsmi), ref _calisanIsmi, value); }
+        }
+
+        [Size(255)]
+        [RuleRegularExpression(DefaultContexts.Save,
+     @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+     CustomMessageTemplate = "Email düzgün formatta değil.")]
+        [RuleRequiredField(DefaultContexts.Save, CustomMessageTemplate = "Bu alan boş olamaz")]
+        public string Email
+        {
+            get { return _email; }
+            set { SetPropertyValue(nameof(Email), ref _email, value); }
+        }
+
+        
+        public DateTime DogumGunu
+        {
+            get => _dogumGunu;
+            set => SetPropertyValue(nameof(DogumGunu), ref _dogumGunu , value);
+        }
+
+        public string Adres { get => _adres; set => SetPropertyValue(nameof(Adres), ref _adres, value); }
+
+        [Size(20)]
+        [RuleRegularExpression(DefaultContexts.Save,
+    @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$",
+    CustomMessageTemplate = "Telefon numarası doğru formatta değil")]
+        [RuleRequiredField(DefaultContexts.Save, CustomMessageTemplate = "Bu alan boş olamaz")]
+        public string PhoneNumber { get => _phoneNumber; set => SetPropertyValue(nameof(PhoneNumber), ref _phoneNumber, value); }
+
+        private Image _image;
+      
+        public Image Image
+        {
+            get { return _image; }
+            set { SetPropertyValue(nameof(Image), ref _image, value); }
+        }
+
+    }
+}
---
+++ finalll.Module/BusinessObjects/Project.cs
@@ -0,0 +1,124 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using static finalll.Module.BusinessObjects.Project;
+
+namespace finalll.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    [Persistent("PROJE")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    [System.ComponentModel.DisplayName("Projeler")]
+    public class Project : XPObject
+    { 
+        public Project(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        public enum ProjeDurumu
+        {
+            ONAY_BEKLIYOR,
+            DEVAM_EDIYOR,
+            TAMAMLANDI
+        }
+
+        public enum ParasalGetiriTipi
+        {
+            GUNLUK,
+            AYLIK,
+            YILLIK
+        }
+
+        public enum ProjeTipi
+        {
+            YURTDISI,
+            TUBITAK,
+            KOBI,
+            ODEV
+        }
+
+        private string _projeAdi;
+        private int _projeNo;
+        private ProjeYurutucusu _projeYurutucusu;
+        private string _projeAmaci;
+        private DateTime _kayitTarihi;
+        private DateTime _projeBaslangici;
+        private DateTime _projeBitisi;
+        private DateTime _tahminiBaslangic;
+        private DateTime _bitis;
+        private ProjeDurumu _projeDurumu;
+        private int _projeGetirisi;
+        private ParasalGetiriTipi _parasalGetiriTipi;
+        private List<Calisan> _users;
+        private List<string> _projeDokumanlari;
+        private ProjeTipi _projeTipi;
+
+        
+        
+        [Association("Project-Milestones")]
+        public XPCollection<KilometreTasi> Milestones
+        {
+            get { return GetCollection<KilometreTasi>(nameof(Milestones)); }
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string ProjeAdi
+        {
+            get { return _projeAdi; }
+            set { SetPropertyValue(nameof(ProjeAdi), ref _projeAdi, value); }
+        }
+
+        public int ProjeNo
+        {
+            get { return _projeNo; }
+            set { SetPropertyValue(nameof(ProjeNo), ref _projeNo, value); }
+        }
+
+        public ProjeYurutucusu ProjeYurutucusu
+        {
+            get { return _projeYurutucusu; }
+            set { SetPropertyValue(nameof(ProjeYurutucusu), ref _projeYurutucusu, value); }
+        }
+
+
+
+
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+
+
+
+
+    }
+}
